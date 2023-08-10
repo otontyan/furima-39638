bf3de1d34a8246c0eb2d2b22036b7573e07ec149
otontyan <poko.akn48@gmail.com>
2023-08-10 20:35:20 +0900

    バリデーションなど実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   validates :nick_name, presence: true
+   validates :family_name_kanji, presence: true
+   validates :personal_name_kanji, presence: true
+   validates :family_name_kana, presence: true
+   validates :personal_name_kana, presence: true
+   validates :birthdate, presence: true
 end
