4724b569e20998a9f21a8e35934ed3d7655dc768
otontyan <poko.akn48@gmail.com>
2023-08-11 21:19:40 +0900

    正規表現とエラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+     validates :family_name_kanji
+     validates :personal_name_kanji
+   end
+
+   with_options presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/ } do
+    validates :family_name_kana
+    validates :personal_name_kana
+  end
+        
    validates :nick_name, presence: true
-   validates :family_name_kanji, presence: true
-   validates :personal_name_kanji, presence: true
-   validates :family_name_kana, presence: true
-   validates :personal_name_kana, presence: true
    validates :birthdate, presence: true
+   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+
+   
 end
