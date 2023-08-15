4cbecb54ff3e83538b4357362bca726498b64bc0
otontyan <poko.akn48@gmail.com>
2023-08-15 18:06:32 +0900

    商品一覧表示機能

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+function price (){
   const priceInput = document.getElementById("item-price");
    priceInput.addEventListener("input", () => {
     const addTaxDom = document.getElementById("add-tax-price");
@@ -6,4 +6,6 @@ window.addEventListener('load', () => {
     const addProfitDom = document.getElementById("profit");
     addProfitDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ))
  })
-});
+};
+
+window.addEventListener('turbo:load', price);
